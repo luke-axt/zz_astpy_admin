23b96bab76b27d1d9af418740a24ff2d12f59611
luke <lc_3030@163.com>
2026-05-27 09:11:54 +0800

    优化rpa.py模板

--- script/template_script/rpa.py
+++ script/template_script/rpa.py
@@ -1,14 +1,12 @@
 import sys
 sys.path.append(r"D:\app\astpy")
-from CoreBussiness.LingXingService import LingXingService
-from common.ResultObj import ResultObj
-from rpa.RpaAdmin import RpaAdmin
-from admin.service.AdminService import AdminService
+
+from zzlc.script.MyAdmin import MyAdminBase
 from utils .BrowserUtils import BrowserUtils
 
 
 
-class xxxxxx(RpaAdmin):
+class xxxxxx(MyAdminBase):
     def __init__(self, jobname):
         super().__init__(jobname)
         self.jobname = jobname
@@ -17,3 +15,20 @@ class xxxxxx(RpaAdmin):
     def action(self, **kwargs):
         browser = BrowserUtils.openChrome(chromdatapath=self.admin.getChromeUserPath(f'{self.__class__.__name__}.action'))
         pass
+
+    def wait_confirm(self)-> bool:
+        """
+        等待用户输入：
+        - 直接回车 = 继续
+        - 输入 Y/y + 回车 = 继续
+        - 其他输入 = 退出
+        """
+        user_input = input("请按回车确认，或输入 Y 继续：").strip().upper()
+        
+        # 空（直接回车）或者 Y 都通过
+        if user_input == "" or user_input == "Y":
+            print("✅ 确认通过，继续执行...")
+            return True
+        else:
+            print("❌ 取消执行")
+            return False
